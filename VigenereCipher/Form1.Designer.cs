354163637de8f187a6274845ffc3ad95a4b19e75
Wenomok <wenomok@gmail.com>
2017-11-10 12:30:56 +0400

    Добавьте файлы проекта.

---
+++ VigenereCipher/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace VigenereCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.encrypt = new System.Windows.Forms.Button();
+            this.decipher = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.secretWord = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // text
+            // 
+            this.text.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.text.Location = new System.Drawing.Point(12, 34);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(666, 226);
+            this.text.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Текст:";
+            // 
+            // encrypt
+            // 
+            this.encrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.encrypt.Location = new System.Drawing.Point(12, 304);
+            this.encrypt.Name = "encrypt";
+            this.encrypt.Size = new System.Drawing.Size(124, 51);
+            this.encrypt.TabIndex = 2;
+            this.encrypt.Text = "Зашифровать";
+            this.encrypt.UseVisualStyleBackColor = true;
+            this.encrypt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // decipher
+            // 
+            this.decipher.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.decipher.Location = new System.Drawing.Point(554, 304);
+            this.decipher.Name = "decipher";
+            this.decipher.Size = new System.Drawing.Size(124, 51);
+            this.decipher.TabIndex = 3;
+            this.decipher.Text = "Расшифровать";
+            this.decipher.UseVisualStyleBackColor = true;
+            this.decipher.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 409);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Кодовое слово:";
+            // 
+            // secretWord
+            // 
+            this.secretWord.Location = new System.Drawing.Point(119, 274);
+            this.secretWord.Name = "secretWord";
+            this.secretWord.Size = new System.Drawing.Size(174, 20);
+            this.secretWord.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 438);
+            this.Controls.Add(this.secretWord);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.decipher);
+            this.Controls.Add(this.encrypt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text);
+            this.Name = "Form1";
+            this.Text = "Шифр Виженера";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button encrypt;
+        private System.Windows.Forms.Button decipher;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox secretWord;
+    }
+}
+
---
+++ VigenereCipher/Form1.cs
@@ -0,0 +1,182 @@
+﻿//Шифр Виженера
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VigenereCipher
+{
+    public partial class Form1 : Form
+    {
+        private static String[,] vigenereSquare;
+        private static String[] alphabetEnglish = 
+        { "A", "B", "C", "D", "E", "F", "G",
+          "H", "I", "J", "K", "L", "M", "N",
+          "O", "P", "Q", "R", "S", "T", "U",
+          "V", "W", "X", "Y", "Z", " ", ".",
+          ",", "!", "?"};
+
+        private static String[] alphabetRussia =
+        {   "А", "Б", "В", "Г", "Д", "Е", "Ё",
+            "Ж", "З", "И", "Й", "К", "Л", "М",
+            "Н", "О", "П", "Р", "С", "Т", "У",
+            "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ",
+            "Ы", "Ь", "Э", "Ю", "Я", " ", ".",
+            ",", "!", "?"};
+
+        List<String> alphabetList;
+
+        private String encryptedText;
+        private String reformedText;
+        private String reformedSecretWord;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(createVigenereSquare())
+            {
+                reformedText = text.Text.ToUpper();
+                reformedSecretWord = secretWord.Text.ToUpper();
+
+                if (reformedText.Length > 0 && reformedSecretWord.Length > 0)
+                {
+                    String encryptedString = "";
+                    int indexOfSecretWordChar = 0;
+                    for (int i = 0; i < reformedText.Length; i++)
+                    {
+                        if (reformedSecretWord.Length > reformedText.Length)
+                        {
+                            indexOfSecretWordChar = i % reformedText.Length;
+                        }
+                        else
+                        {
+                            indexOfSecretWordChar = i % reformedSecretWord.Length;
+                        }
+
+
+                        encryptedString += vigenereSquare[
+                            alphabetList.IndexOf(reformedSecretWord.ElementAt(indexOfSecretWordChar).ToString()),
+                            alphabetList.IndexOf(reformedText.ElementAt(i).ToString())];
+                    }
+                    encryptedText = encryptedString;
+                    MessageBox.Show(encryptedText, "Зашифровано", MessageBoxButtons.OK);
+
+                    //EncryptForm encryptForm = new EncryptForm();
+
+                    //encryptForm.receivedText = text.Text;
+                    //encryptForm.receivedSecretWord = secretWord.Text;
+                    //encryptForm.vigenereSquareEncryptForms = vigenereSquare;
+
+                    //encryptForm.Show();
+                }
+                else
+                {
+                    label2.Text = "Требуемые поля необходимо заполнить";
+                }
+            } else
+            {
+                label2.Text = "Данный язык не поддерживается";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (text.Text.Length > 0 && secretWord.Text.Length > 0)
+            {
+                if(encryptedText != null && text.Text.ToUpper().Equals(reformedText))
+                {
+                    String decryptedString = "";
+                    for (int i = 0; i < encryptedText.Length; i++)
+                    {
+                        int indexOfSecretWordChar = i % reformedSecretWord.Length;
+
+                        int indexOfCurrentLine = alphabetList.IndexOf(reformedSecretWord.ElementAt(indexOfSecretWordChar).ToString());
+                        int indexOfDecryptedChar = 0;
+
+                        for (int j = 0; j < alphabetList.Count; j++)
+                        {
+                            if (vigenereSquare[indexOfCurrentLine, j].Equals(encryptedText.ElementAt(i).ToString()))
+                            {
+                                indexOfDecryptedChar = j;
+                            }
+                        }
+
+                        decryptedString += alphabetList.ElementAt(indexOfDecryptedChar);
+                    }
+                    MessageBox.Show(decryptedString, "Расшифровано", MessageBoxButtons.OK);
+                } else
+                {
+                    label2.Text = "Для начала зашифруйте текст";
+                }
+                
+            }
+            else
+            {
+                label2.Text = "Требуемые поля необходимо заполнить";
+            }
+        }
+
+        private bool createVigenereSquare() {
+            alphabetList = new List<String>(alphabetEnglish);
+            if (alphabetList.Contains(text.Text.ElementAt(0).ToString().ToUpper()))
+            {
+                setVigenereSquareLanguageWithAlphabet(alphabetEnglish);
+                //vigenereSquare = new String[alphabetEnglish.Length, alphabetEnglish.Length];
+                //for (int i = 0; i < alphabetEnglish.Length; i++)
+                //{
+                //    alphabetList = new List<string>(alphabetEnglish);
+                //    for (int j = 0; j < alphabetEnglish.Length; j++)
+                //    {
+                //        vigenereSquare[i, j] = alphabetEnglish[j];
+                //    }
+                //    alphabetList.Insert(alphabetEnglish.Length, alphabetEnglish.ElementAt(0));
+                //    alphabetList.RemoveAt(0);
+                //    alphabetEnglish = alphabetList.ToArray();
+                //}
+                return true;
+            }
+            else
+            {
+                alphabetList = new List<string>(alphabetRussia);
+                if (alphabetList.Contains(text.Text.ElementAt(0).ToString().ToUpper()))
+                {
+                    setVigenereSquareLanguageWithAlphabet(alphabetRussia);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void setVigenereSquareLanguageWithAlphabet(String[] alphabet)
+        {
+            vigenereSquare = new String[alphabet.Length, alphabet.Length];
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                alphabetList = new List<string>(alphabet);
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    vigenereSquare[i, j] = alphabet[j];
+                }
+                alphabetList.Insert(alphabet.Length, alphabet.ElementAt(0));
+                alphabetList.RemoveAt(0);
+                alphabet = alphabetList.ToArray();
+            }
+        }
+    }
+}
